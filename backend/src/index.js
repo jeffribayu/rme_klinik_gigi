a9b33d923fc6d5f1134712a5e8799ddfd189f5d7
jeffribayu <jeffsaja0@gmail.com>
2026-08-07 12:51:59 +0700

    Perbaikan koneksi database

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,6 +33,10 @@ app.use(
 
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
+app.use((_req, res, next) => {
+  res.setHeader('X-Robots-Tag', 'noindex, nofollow, noarchive, nosnippet');
+  next();
+});
 
 const uploadsPath = path.join(__dirname, '../uploads');
 app.use('/uploads', express.static(uploadsPath));
